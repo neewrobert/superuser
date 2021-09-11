03cf4a65f7d9448d4b9bde95769f87cd5234e053
Newton Santos <neewrobert@gmail.com>
2021-09-11 17:32:40 -0300

    initializer

---
+++ src/main/java/com/neewrobert/superuser/SuperuserApplication.java
@@ -0,0 +1,13 @@
+package com.neewrobert.superuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperuserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperuserApplication.class, args);
+	}
+
+}
