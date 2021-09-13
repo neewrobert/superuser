db26249cb0760d50665a1697324a06a5212caa4d
Newton Santos <neewrobert@gmail.com>
2021-09-13 18:39:23 -0300

    Config classes and DTO Pattern with tests

---
+++ src/test/java/com/neewrobert/superuser/dto/UserDTOTest.java
@@ -0,0 +1,53 @@
+package com.neewrobert.superuser.dto;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+
+import com.neewrobert.superuser.model.Profile;
+import com.neewrobert.superuser.model.User;
+
+public class UserDTOTest {
+	
+	private ModelMapper modelMapper = new ModelMapper();
+	
+	
+	@Test
+	public void testMapperToDTO() {
+		
+		Profile profile = new Profile();
+		profile.setProfileType("manager");
+		
+		User entity = new User("John Doe", LocalDate.of(1991, 07, 28), profile, "987411289", "johndoe@gmail.com");
+		
+		UserDTO dto = modelMapper.map(entity, UserDTO.class);
+		
+		assertEquals(dto.getName(), entity.getName());
+		assertEquals(dto.getBirthDate(), entity.getBirthDate());
+		assertEquals(dto.getProfileType(), entity.getProfile().getProfileType());
+		assertEquals(dto.getPhoneNumber(), entity.getPhoneNumber());
+		assertEquals(dto.getEmail(), entity.getEmail());
+		
+	}
+	
+	@Test
+	public void testMapperToEntity() {
+		
+		Profile profile = new Profile();
+		profile.setProfileType("manager");
+		UserDTO dto = new UserDTO("John Doe", LocalDate.of(1991, 07, 28), profile.getProfileType(), "987411289", "johndoe@gmail.com");
+		
+		User entity = modelMapper.map(dto, User.class);
+		
+		assertEquals(entity.getName(), dto.getName());
+		assertEquals(entity.getBirthDate(), dto.getBirthDate());
+		assertEquals(entity.getProfile().getProfileType(), dto.getProfileType());
+		assertEquals(entity.getPhoneNumber(), dto.getPhoneNumber());
+		assertEquals(entity.getEmail(), dto.getEmail());
+		
+	}
+
+}
